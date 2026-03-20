b92a29e38d6b82238236c4a2124cd7783a88ae7d
John Doe <johndoe@example.com>
2026-03-20 20:40:40 +0300

    ald

---
+++ projects.js
@@ -0,0 +1,12 @@
+window.PROJECTS = [
+  {
+    "title": "24 Clock",
+    "folder": "24_clock",
+    "path": "24_clock/index.html"
+  },
+  {
+    "title": "ASCII 3D",
+    "folder": "ASCII_3D",
+    "path": "ASCII_3D/index.html"
+  }
+];
