017b6e9255566b2614a474fdb9c6abf6be0e776b
John Doe <johndoe@example.com>
2026-03-27 15:50:28 +0300

    asgard

--- projects.js
+++ projects.js
@@ -2,6 +2,7 @@ window.PROJECTS = [
   {
     "title": "24 Hour Clock",
     "path": "24_hour_clock/index.html",
+    "readmePath": "24_hour_clock/README.md",
     "createdAt": "2026-03-10",
     "updatedAt": "2026-03-22",
     "topic": "-"
@@ -9,6 +10,7 @@ window.PROJECTS = [
   {
     "title": "3D ASCII Render",
     "path": "3d_ascii_render/index.html",
+    "readmePath": "3d_ascii_render/README.md",
     "createdAt": "2025-01-18",
     "updatedAt": "2026-03-21",
     "topic": "sim"
@@ -16,6 +18,7 @@ window.PROJECTS = [
   {
     "title": "Butterfly Effect",
     "path": "butterfly_effect/index.html",
+    "readmePath": "butterfly_effect/README.md",
     "createdAt": "2026-03-22",
     "updatedAt": "2026-03-22",
     "topic": "sim"
@@ -23,6 +26,7 @@ window.PROJECTS = [
   {
     "title": "Game Of Life On Toroid",
     "path": "game_of_life_on_toroid/index.html",
+    "readmePath": "game_of_life_on_toroid/README.md",
     "createdAt": "2026-03-20",
     "updatedAt": "2026-03-22",
     "topic": "art"
@@ -30,6 +34,7 @@ window.PROJECTS = [
   {
     "title": "Lorentz Attractor",
     "path": "lorentz_attractor/index.html",
+    "readmePath": "lorentz_attractor/README.md",
     "createdAt": "2026-03-25",
     "updatedAt": "2026-03-25",
     "topic": "art"
@@ -37,6 +42,7 @@ window.PROJECTS = [
   {
     "title": "Optical Illusions",
     "path": "optical_illusions/index.html",
+    "readmePath": "optical_illusions/README.md",
     "createdAt": "2026-03-25",
     "updatedAt": "2026-03-25",
     "topic": "-"
